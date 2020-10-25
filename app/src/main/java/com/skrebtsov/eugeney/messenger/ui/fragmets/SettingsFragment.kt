2c8ab0131ff9f89e406cd254b3581f9f21f46d21
EugeneyC <xxtedixx@yandex.ru>
2020-10-25 18:46:23 +0300

    create action menu

--- app/src/main/java/com/skrebtsov/eugeney/messenger/ui/fragmets/SettingsFragment.kt
+++ app/src/main/java/com/skrebtsov/eugeney/messenger/ui/fragmets/SettingsFragment.kt
@@ -1,7 +1,17 @@
 package com.skrebtsov.eugeney.messenger.ui.fragmets
 
+import android.view.Menu
+import android.view.MenuInflater
 import com.skrebtsov.eugeney.messenger.R
 
 class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
 
+    override fun onResume() {
+        super.onResume()
+        setHasOptionsMenu(true)
+    }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        activity?.menuInflater?.inflate(R.menu.settings_action_menu, menu)
+    }
 }
